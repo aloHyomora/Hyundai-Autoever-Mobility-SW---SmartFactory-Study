4013e346bb3b09b42ab2aa3c3467349903e67bb0
aloHyomora <alohomoratolit@naver.com>
2025-12-20 21:06:41 +0900

    edit: folder architecture

---
+++ DataAnalysis/Task 251219/Sripts/test.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main():
+    print("Hello, World!")
+    print("This is a test script.")
+    print("Arguments passed to the script:", sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
